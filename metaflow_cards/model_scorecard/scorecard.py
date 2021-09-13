2279cb7a78ab686b086b7232017c3d74f120a96e
Valay Dave <valaygaurang@gmail.com>
2021-09-13 11:11:17 -0700

    Tiny tweek to add arguments for card

--- metaflow_cards/model_scorecard/scorecard.py
+++ metaflow_cards/model_scorecard/scorecard.py
@@ -2,11 +2,19 @@ from metaflow.plugins.card_modules.card import MetaflowCard
 
 class ScoreCard(MetaflowCard):
     name='custom_score_card'
+    def __init__(self,footer=False) -> None:
+        self._footer = footer
     
     def render(self, task):
         mustache = self._get_mustache()
-        TEMPLATE = f"""
-        THIS IS A CUSTOM SCORE CARD
-        """
+        TEMPLATE = """
+        %s
+        """%(task.id)
+        if self._footer: 
+            TEMPLATE = """
+        %s
+        With Footer
+        """%(task.id)
+            
         return mustache.render(TEMPLATE)
         
